aefdfb95449009aca877f0ce48b001f6a361a8de
MariaCuenca26 <86026443+MariaCuenca26@users.noreply.github.com>
2021-12-26 20:21:43 -0500

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,11 @@
+#operadores aritmeticos en python
+numero1= int(input("Ingrese el  numero1: "))
+numero2 = int(input("Ingrese el numero2: "))
+suma = numero1+numero2
+resta = numero1-numero2
+multiplicacion= numero1*numero2
+division = numero1/ numero2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
